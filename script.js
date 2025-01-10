fe67639b6d2e2762ac454e97a7ff5245da0b9055
Laura Reķe <18744732-LauraReke@users.noreply.replit.com>
2025-01-10 12:11:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let vards="GALAXY";
+console.log(vards);
+x=4;
+  y=4;
+z=7;
+alert("galaxy atsperīgi dejo");
+prompt("Vai zini, kā sauc galaxy?");
+
+ 
+
+
+
+
+
+
+
+
+function h1izveide(){
+let virsraksts=document.createElement("h1"); /*Izveido virsraksta tagu */
+virsraksts.innerHTML= "URY@B8nhMTMHNUJUMVtid";
+  /* Maina izveidotā virsraksta taga saturu */
+document.body.appendChild(virsraksts); /*Pevieno virsrakstu*/
+  virsraksts.style.color="green";
+}
+function ciparo(){
+  for(i=1; i<5; i++){
+    console.log(i);}
+  }
+function rakstit(){
+  for(i=0; i<5; i++){
+    let rindkopa=document.createElement("p");
+    rindkopa.innerHTML ="Šī ir" + i +". rindkopa.";
+    document.body.appendChild(rindkopa);
+  }
+}
+function rakstit() {
+  for (i = 0; i < 5; i++) {
+    let rindkopa = document.createElement("p");
+    
+    rindkopa.innerHTML = "Mani sauc GALAXY"; 
+    document.body.appendChild(rindkopa);
+  } }
+function vardosana(){
+  let name=prompt("Ievadi savu vārdu!");
+  let skaits=prompt("Ievadi skaitli:");
+  for (i=0; i<skaits; i++){
+    let rindkopa= document.createElement("p");
+  rindkopa.HTML="Mans vārds ir"  + name;
+     document.body.appenedChild(rindkopa);
+  {
+}}
